3c4e7ee973ef4f5c551d21f212248839e529424c
Douglas Cunha <dcunha1@wal-mart.com>
2015-04-20 11:07:31 -0300

    .

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 var methodOverride = require('method-override')
-var host = process.env.OPENSHIFT_NODEJS_IP;
-var port =  parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080;
+var host = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var routes = require('./routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
